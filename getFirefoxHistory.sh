c5d7b32b308faa303835ff4b2cb6de2e3914472e
Michaël Martinez <mmartinem7f@gmail.com>
2016-10-23 03:07:31 +0200

    Minor update

--- getFirefoxHistory.sh
+++ getFirefoxHistory.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-	OUTPUT_FILE="C:\Users\utilisateur\Desktop\firefox_history.txt"
+	OUTPUT_FILE="~/firefox_history.txt"
 else
-	OUTPUT_FILE="C:\Users\utilisateur\Desktop\\"$1
+	OUTPUT_FILE="~/"$1
 fi
 
 python2 <<< $'import json\nf = open("C:/Documents and Settings/utilisateur/Application Data/Mozilla/Firefox/Profiles/299rsaq0.default/sessionstore.js", "r")\njdata = json.loads(f.read())\nf.close()\nfor win in jdata.get("windows"):\n\tfor tab in win.get("tabs"):\n\t\ti = tab.get("index") - 1\n\t\tprint tab.get("entries")[i].get("url")' > $OUTPUT_FILE
